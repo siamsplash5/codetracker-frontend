0bad587ebeb27db778d6f99eb3bf544631795a3e
siamsplash5 <siamsplash52@gmail.com>
2023-06-25 13:15:41 +0600

    feat: added timer to contest page

--- src/features/contests/components/Dashboard.jsx
+++ src/features/contests/components/Dashboard.jsx
@@ -2,21 +2,19 @@ import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock, faSignInAlt } from "@fortawesome/free-solid-svg-icons";
 
-export default function ContestDashboard ({contest: cl}) {
-    // Sample contest data
-    const contest = {
-        title: cl.title,
-        description: cl.description,
-        startDate: new Date("2023-07-01T09:00:00Z"),
-        endDate: new Date("2023-07-02T17:00:00Z"),
-    };
-
+export default function ContestDashboard ({contest}) {
     const [countdown, setCountdown] = useState(calculateCountdown());
 
     // Calculate countdown time
-    function calculateCountdown() {
-        const now = new Date().getTime();
-        const distance = contest.startDate.getTime() - now;
+    function calculateCountdown(contestStatus) {
+        const now = new Date();
+        let distance = 0;
+
+        if(contestStatus==="Upcoming"){
+            distance = contest.beginTime - now;
+        }else if(contestStatus==="Running"){
+            distance = contest.beginTime + contest.contestLength - now;
+        }
 
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor(
@@ -25,12 +23,30 @@ export default function ContestDashboard ({contest: cl}) {
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        if(days){
+            return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        }
+        if (hours) {
+            return `${hours}h ${minutes}m ${seconds}s`;
+        }
+        if (minutes) {
+            return `${minutes}m ${seconds}s`;
+        }
+        if(seconds){
+            return `${seconds}s`;
+        }
     }
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCountdown(calculateCountdown());
+            if(contest.beginTime>Date.now()){
+                setCountdown(calculateCountdown("Upcoming"));
+            }if (
+                contest.beginTime + contest.contestLength > Date.now() &&
+                contest.beginTime <= Date.now()
+            ) {
+                setCountdown(calculateCountdown("Running"));
+            }
         }, 1000);
 
         return () => {
@@ -46,46 +62,70 @@ export default function ContestDashboard ({contest: cl}) {
             <p className="text-lg text-gray-700 text-center mb-8">
                 {contest.description}
             </p>
-            <div className="flex flex-col items-center mb-8">
-                <div className="flex items-center mb-4">
-                    <FontAwesomeIcon
-                        icon={faClock}
-                        className="text-gray-500 mr-2"
-                    />
-                    <p className="text-gray-700">
-                        Starts in:
-                        <span className="font-bold ml-1">{countdown}</span>
-                    </p>
-                </div>
-                <div className="flex items-center">
-                    <FontAwesomeIcon
-                        icon={faClock}
-                        className="text-gray-500 mr-2"
-                    />
-                    <p className="text-gray-700">
-                        Ends on:
-                        <span className="font-bold ml-1">
-                            {contest.endDate.toLocaleString()}
-                        </span>
-                    </p>
-                </div>
+            <div className="flex flex-col items-center mb-4">
+                {contest.beginTime > Date.now() && (
+                    <div className="flex items-center text-xl">
+                        <FontAwesomeIcon
+                            icon={faClock}
+                            className="text-gray-500 mr-2"
+                        />
+                        <p className="text-gray-700">
+                            Starts in:
+                            <span className="font-bold ml-1">{countdown}</span>
+                        </p>
+                    </div>
+                )}
+                {contest.beginTime + contest.contestLength > Date.now() &&
+                    contest.beginTime <= Date.now() && (
+                        <div className="text-center">
+                            <div className="items-center text-xl mx-8 mb-3">
+                                <span className="text-gray-700">
+                                    Contest is running...
+                                </span>
+                            </div>
+                            <div className="flex item-center text-xl">
+                                <FontAwesomeIcon
+                                    icon={faClock}
+                                    className="text-gray-500 mr-2"
+                                />
+                                <p className="text-gray-700">
+                                    Remaining:
+                                    <span className="font-bold ml-1">
+                                        {countdown}
+                                    </span>
+                                </p>
+                            </div>
+                        </div>
+                    )}
+                {contest.beginTime + contest.contestLength < Date.now() && (
+                    <div className="flex items-center text-xl">
+                        <p className="text-gray-700">
+                            The Contest has finished
+                        </p>
+                    </div>
+                )}
             </div>
-            <div className="flex flex-col items-center">
-                <form className="flex flex-col items-center">
-                    <input
-                        type="password"
-                        placeholder="Enter contest password"
-                        className="px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-blue-500"
-                    />
-                    <button
-                        type="submit"
-                        className="px-6 py-3 bg-blue-500 text-white rounded-md flex items-center transition-colors duration-300 hover:bg-blue-600 focus:outline-none"
-                    >
-                        <FontAwesomeIcon icon={faSignInAlt} className="mr-2" />
-                        Sign Up
-                    </button>
-                </form>
-            </div>
+            {contest.privacy !== "Public" && (
+                <div className="flex flex-col items-center">
+                    <form className="flex flex-col items-center">
+                        <input
+                            type="password"
+                            placeholder="Enter contest password"
+                            className="px-4 py-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-blue-500"
+                        />
+                        <button
+                            type="submit"
+                            className="px-6 py-3 bg-blue-500 text-white rounded-md flex items-center transition-colors duration-300 hover:bg-blue-600 focus:outline-none"
+                        >
+                            <FontAwesomeIcon
+                                icon={faSignInAlt}
+                                className="mr-2"
+                            />
+                            Sign Up
+                        </button>
+                    </form>
+                </div>
+            )}
         </div>
     );
 };
