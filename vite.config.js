41494c06f80e0e5898e982181f2e13730cdfe19e
siamsplash5 <siamsplash52@gmail.com>
2023-07-13 05:27:57 +0600

    fix: bugs

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-// import react from '@vitejs/plugin-react-swc'
-// import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'https://codetracker-backend.onrender.com', // Replace with your backend API URL
-//         changeOrigin: true,
-//         secure: true, // Set to true if your backend server uses HTTPS
-//       },
-//     },
-//   },
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://codetracker-backend.onrender.com', // Replace with your backend API URL
+  //       changeOrigin: true,
+  //       secure: true, // Set to true if your backend server uses HTTPS
+  //     },
+  //   },
+  // },
+})
