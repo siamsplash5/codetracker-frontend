8324d5512647bde84cd42d7877241a876cc31807
siamsplash5 <siamsplash52@gmail.com>
2023-07-13 20:22:58 +0600

    fix:

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://codetracker-backend.onrender.com', // Replace with your backend API URL
-  //       changeOrigin: true,
-  //       secure: true, // Set to true if your backend server uses HTTPS
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://codetracker-backend.onrender.com', // Replace with your backend API URL
+        changeOrigin: true,
+        secure: true, // Set to true if your backend server uses HTTPS
+      },
+    },
+  },
 })
