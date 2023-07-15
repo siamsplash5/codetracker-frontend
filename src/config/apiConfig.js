fca6e83eec5dc04ac7d59ed78accbacdf8c6215f
siamsplash5 <siamsplash52@gmail.com>
2023-07-15 18:59:09 +0600

    fix:

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,27 +1,27 @@
 const backendURL = 'https://codetracker-backend.vercel.app';
             
 const apiEndPoints = {
-    register: `${backendURL}/api/register/`,
-    verifyRegistration: `${backendURL}/api/register-verify/`,
-    login: `${backendURL}/api/login/`,
-    logout: '/api/logout/',
-    passwordUpdate: `${backendURL}/api/reset-password/`,
-    passwordUpdateVerify: `${backendURL}/api/reset-password-verify/`,
-    registerContest: `${backendURL}/api/contest/register/`,
-    createContest: `${backendURL}/api/contest/create/`,
+    register: `/register/`,
+    verifyRegistration: `/register-verify/`,
+    login: `/login/`,
+    logout: `/logout/`,
+    passwordUpdate: `/reset-password/`,
+    passwordUpdateVerify: `/reset-password-verify/`,
+    registerContest: `/contest/register/`,
+    createContest: `/contest/create/`,
     // will include contest ID
-    contestStandingByContestID: `${backendURL}/api/contest-query/standings/`,
+    contestStandingByContestID: `/contest-query/standings/`,
     // will include contest ID
-    contestSubmissionsByContestID: `${backendURL}/api/submissions/specific-contest/`,
-    problemAll: `${backendURL}/api/problem-all`,
-    contestAll: `${backendURL}/api/contest-query/all`,
+    contestSubmissionsByContestID: `/submissions/specific-contest/`,
+    problemAll: `/problem-all`,
+    contestAll: `/contest-query/all`,
     // will include judge, problem ID, contestID
-    submissionOfSpecificProblem: `${backendURL}/api/submissions/specific-problem/`,
-    contestProblems: `${backendURL}/api/contest-problem/all/`,
-    submit: `${backendURL}/api/submit`,
-    problem: `${backendURL}/api/problem`,
+    submissionOfSpecificProblem: `/submissions/specific-problem/`,
+    contestProblems: `/contest-problem/all/`,
+    submit: `/submit`,
+    problem: `/problem`,
     // will include username
-    submissionsByUser: `${backendURL}/api/submissions/specific-user/`,
+    submissionsByUser: `/submissions/specific-user/`,
 }
 
 export default apiEndPoints;
