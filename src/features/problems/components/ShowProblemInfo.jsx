a2576d9c28ad42f59473258ec31b3b8ac7d2b233
siamsplash5 <siamsplash52@gmail.com>
2023-08-15 07:05:57 +0600

    style: added tooltip for problem page

--- src/features/problems/components/ShowProblemInfo.jsx
+++ src/features/problems/components/ShowProblemInfo.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ReactHtmlParser from "react-html-parser";
 import { LightBulbYellow } from "../../../components/Icons";
+import { Tooltip } from "react-tooltip";
 
 export default function ShowProblemInfo({ problem }) {
     const [show, setShow] = useState(false);
@@ -16,102 +17,118 @@ export default function ShowProblemInfo({ problem }) {
     } = problem;
 
     return (
-        <div className="container text-center rounded-xl mb-2 text-slate-50 bg-cyan-950 px-4 py-4">
-            <button
-                className=""
-                onClick={() => (show ? setShow(false) : setShow(true))}
-            >
-               <LightBulbYellow /> {show ? "Hide Problem Information" : "Show Problem Information"}
-            </button>
-            {show && (
-                <div>
-                    <br />
-                    <div className="text-left">
-                        {sourceLimit && (
-                            <p>
-                                <span className="text-amber-300">
-                                    Source Limit:{" "}
-                                </span>
-                                {sourceLimit}
-                            </p>
-                        )}
-                        {rating && (
-                            <p>
-                                <span className="text-amber-300">Rating: </span>
-                                <span>
-                                    <b>{rating}</b>
-                                </span>
-                            </p>
-                        )}
-                        {score && (
-                            <p>
-                                <span className="text-amber-300">Score: </span>
-                                <span>
-                                    <b>{score}</b>
-                                </span>
-                            </p>
-                        )}
-                        {difficulty && (
-                            <p>
-                                <span className="text-amber-300">
-                                    Difficulty:{" "}
-                                </span>
-                                <span>
-                                    <b>{difficulty}</b>
-                                </span>
-                            </p>
-                        )}
-                        {tags && tags.length > 0 && (
-                            <p>
-                                <span className="text-amber-300">Tags: </span>
-                                {tags.map((element, index) => (
-                                    <li key={index}>{element}</li>
-                                ))}
-                            </p>
-                        )}
-                        {(() => {
-                            if (author && author.length > 0) {
-                                if (judge === "Timus") {
+        <>
+            <Tooltip anchorSelect="#problemInfo" place="bottom">
+                {`${show? `Hide`: `Show`} problem's rating, author, tags, source etc.`}
+            </Tooltip>
+            <div className="container text-center rounded-xl mb-2 text-slate-50 bg-cyan-950 px-4 py-4">
+                <button
+                    onClick={() => (show ? setShow(false) : setShow(true))}
+                    id="problemInfo"
+                >
+                    <LightBulbYellow />{" "}
+                    {show
+                        ? "Hide Problem Information"
+                        : "Show Problem Information"}
+                </button>
+                {show && (
+                    <div>
+                        <br />
+                        <div className="text-left">
+                            {sourceLimit && (
+                                <p>
+                                    <span className="text-amber-300">
+                                        Source Limit:{" "}
+                                    </span>
+                                    {sourceLimit}
+                                </p>
+                            )}
+                            {rating && (
+                                <p>
+                                    <span className="text-amber-300">
+                                        Rating:{" "}
+                                    </span>
+                                    <span>
+                                        <b>{rating}</b>
+                                    </span>
+                                </p>
+                            )}
+                            {score && (
+                                <p>
+                                    <span className="text-amber-300">
+                                        Score:{" "}
+                                    </span>
+                                    <span>
+                                        <b>{score}</b>
+                                    </span>
+                                </p>
+                            )}
+                            {difficulty && (
+                                <p>
+                                    <span className="text-amber-300">
+                                        Difficulty:{" "}
+                                    </span>
+                                    <span>
+                                        <b>{difficulty}</b>
+                                    </span>
+                                </p>
+                            )}
+                            {tags && tags.length > 0 && (
+                                <p>
+                                    <span className="text-amber-300">
+                                        Tags:{" "}
+                                    </span>
+                                    {tags.map((element, index) => (
+                                        <li key={index}>{element}</li>
+                                    ))}
+                                </p>
+                            )}
+                            {(() => {
+                                if (author && author.length > 0) {
+                                    if (judge === "Timus") {
+                                        return (
+                                            <p>
+                                                <span className="text-amber-300">
+                                                    Information:
+                                                </span>
+                                                <ul>
+                                                    {author.map(
+                                                        (element, index) => (
+                                                            <li key={index}>
+                                                                {element}
+                                                            </li>
+                                                        )
+                                                    )}
+                                                </ul>
+                                            </p>
+                                        );
+                                    }
+
                                     return (
-                                        <p>
+                                        <div>
                                             <span className="text-amber-300">
-                                                Information:
-                                            </span>
-                                            <ul>
-                                                {author.map(
-                                                    (element, index) => (
-                                                        <li key={index}>
-                                                            {element}
-                                                        </li>
-                                                    )
-                                                )}
-                                            </ul>
-                                        </p>
+                                                Author:
+                                            </span>{" "}
+                                            {ReactHtmlParser(author)}
+                                        </div>
                                     );
                                 }
+                            })()}
 
-                                return (
-                                    <div>
-                                        <span className="text-amber-300">
-                                            Author:
-                                        </span>{" "}
-                                        {ReactHtmlParser(author)}
-                                    </div>
-                                );
-                            }
-                        })()}
-
-                        {source && (
-                            <p>
-                                <span className="text-amber-300">Source: </span>{" "}
-                                <a href={source} target="_blank">
-                                    {judge}
-                                </a>
-                            </p>
-                        )}
+                            {source && (
+                                <p>
+                                    <span className="text-amber-300">
+                                        Source:{" "}
+                                    </span>{" "}
+                                    <a href={source} target="_blank">
+                                        {judge}
+                                    </a>
+                                </p>
+                            )}
+                        </div>
                     </div>
-                </div>
-            )}
-        </div>
+                )}
+            </div>
+        </>
     );
 }
--- src/features/submissions/components/SubmitSolution.jsx
+++ src/features/submissions/components/SubmitSolution.jsx
@@ -6,6 +6,7 @@ import AtcoderOptions from "./AtcoderOptions";
 import CodeforcesOptions from "./CodeforcesOptions";
 import SpojOptions from "./SpojOptions";
 import TimusOptions from "./TimusOptions";
+import { Tooltip } from "react-tooltip";
 
 export default function SubmitSolution({ handle, judge }) {
     const [showSubmitModal, setShowSubmitModal] = useState(false);
@@ -47,109 +48,115 @@ export default function SubmitSolution({ handle, judge }) {
     }, [showSubmitModal]);
 
     return (
-        <div className="mb-5">
-            <button
-                type="button"
-                className="w-full text-white bg-indigo-800 hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-buttons dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                onClick={handleSubmit}
-            >
-               <PaperPlaneIconDefault /> Submit
-            </button>
-            {showSubmitModal && (
-                <>
-                    <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                    >
-                        <div className="relative w-full my-6 mx-auto max-w-4xl">
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col bg-white outline-none focus:outline-none">
-                                <div className="flex items-start justify-between p-6 border-b border-solid border-gray-300 rounded-t">
-                                    <h3 className="text-3xl font-semibold">
-                                        Submit Solution
-                                    </h3>
-                                    <button
-                                        className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                        onClick={() =>
-                                            setShowSubmitModal(false)
-                                        }
-                                    >
-                                        X
-                                    </button>
-                                </div>
-
-                                <form onSubmit={handleSolutionSubmit}>
-                                    <div className="relative p-6 flex-auto">
-                                        <div className="mb-6">
-                                            <label
-                                                className="block text-gray-700 text-lg font-bold mb-2"
-                                                htmlFor="language"
-                                            >
-                                                Language
-                                            </label>
-                                            <select
-                                                id="language"
-                                                name="language"
-                                                className="appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-lg"
-                                                onChange={handleLangIDChange}
-                                                required
-                                                defaultValue=""
-                                            >
-                                                {judge === "Atcoder" && (
-                                                    <AtcoderOptions />
-                                                )}
-                                                {judge === "Codeforces" && (
-                                                    <CodeforcesOptions />
-                                                )}
-                                                {judge === "Timus" && (
-                                                    <TimusOptions />
-                                                )}
-                                                {judge === "Spoj" && (
-                                                    <SpojOptions />
-                                                )}
-                                            </select>
-                                        </div>
-                                        <div className="mb-6">
-                                            <label
-                                                className="block text-gray-700 text-lg font-bold mb-2"
-                                                htmlFor="solution"
-                                            >
-                                                Solution
-                                            </label>
-                                            <textarea
-                                                id="solution"
-                                                name="solution"
-                                                className="appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-lg"
-                                                rows={10}
-                                                onChange={
-                                                    handleSourceCodeChange
-                                                }
-                                                required
-                                            ></textarea>
-                                        </div>
-                                    </div>
-                                    <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
+        <>
+            <Tooltip anchorSelect="#submit" place="bottom">
+                Click here to submit your solution
+            </Tooltip>
+            <div className="mb-5">
+                <button
+                    type="button"
+                    className="w-full text-white bg-indigo-800 hover:bg-indigo-900 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-buttons dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    id="submit"
+                    onClick={handleSubmit}
+                >
+                    <PaperPlaneIconDefault /> Submit
+                </button>
+                {showSubmitModal && (
+                    <>
+                        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                            <div className="relative w-full my-6 mx-auto max-w-4xl">
+                                <div className="border-0 rounded-lg shadow-lg relative flex flex-col bg-white outline-none focus:outline-none">
+                                    <div className="flex items-start justify-between p-6 border-b border-solid border-gray-300 rounded-t">
+                                        <h3 className="text-3xl font-semibold">
+                                            Submit Solution
+                                        </h3>
                                         <button
-                                            className="text-red-500 background-transparent font-bold uppercase px-6 py-3 text-lg outline-none focus:outline-none mr-2 mb-1 ease-linear transition-all duration-150"
-                                            type="button"
+                                            className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                                             onClick={() =>
                                                 setShowSubmitModal(false)
                                             }
                                         >
-                                            Close
-                                        </button>
-                                        <button
-                                            className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-lg px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-2 mb-1 ease-linear transition-all duration-150"
-                                            type="submit"
-                                        >
-                                            Submit
+                                            X
                                         </button>
                                     </div>
-                                </form>
+
+                                    <form onSubmit={handleSolutionSubmit}>
+                                        <div className="relative p-6 flex-auto">
+                                            <div className="mb-6">
+                                                <label
+                                                    className="block text-gray-700 text-lg font-bold mb-2"
+                                                    htmlFor="language"
+                                                >
+                                                    Language
+                                                </label>
+                                                <select
+                                                    id="language"
+                                                    name="language"
+                                                    className="appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-lg"
+                                                    onChange={
+                                                        handleLangIDChange
+                                                    }
+                                                    required
+                                                    defaultValue=""
+                                                >
+                                                    {judge === "Atcoder" && (
+                                                        <AtcoderOptions />
+                                                    )}
+                                                    {judge === "Codeforces" && (
+                                                        <CodeforcesOptions />
+                                                    )}
+                                                    {judge === "Timus" && (
+                                                        <TimusOptions />
+                                                    )}
+                                                    {judge === "Spoj" && (
+                                                        <SpojOptions />
+                                                    )}
+                                                </select>
+                                            </div>
+                                            <div className="mb-6">
+                                                <label
+                                                    className="block text-gray-700 text-lg font-bold mb-2"
+                                                    htmlFor="solution"
+                                                >
+                                                    Solution
+                                                </label>
+                                                <textarea
+                                                    id="solution"
+                                                    name="solution"
+                                                    className="appearance-none border rounded w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline text-lg"
+                                                    rows={10}
+                                                    onChange={
+                                                        handleSourceCodeChange
+                                                    }
+                                                    required
+                                                ></textarea>
+                                            </div>
+                                        </div>
+                                        <div className="flex items-center justify-end p-6 border-t border-solid border-gray-300 rounded-b">
+                                            <button
+                                                className="text-red-500 background-transparent font-bold uppercase px-6 py-3 text-lg outline-none focus:outline-none mr-2 mb-1 ease-linear transition-all duration-150"
+                                                type="button"
+                                                onClick={() =>
+                                                    setShowSubmitModal(false)
+                                                }
+                                            >
+                                                Close
+                                            </button>
+                                            <button
+                                                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-lg px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-2 mb-1 ease-linear transition-all duration-150"
+                                                type="submit"
+                                            >
+                                                Submit
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-                </>
-            )}
-        </div>
+                        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                    </>
+                )}
+            </div>
+        </>
     );
 }
